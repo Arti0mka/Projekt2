60cb5e7a577b4a064127eb204948e0f78a69ab5f
Arti0mka <74427717+Arti0mka@users.noreply.github.com>
2021-07-27 16:10:43 +0300

    Update tele4.py

--- tele4.py
+++ tele4.py
@@ -1,1 +1,7 @@
+import random
+num = input('login ')
+pas = ''
+for x in range(4): #Количество символов (4)
+    pas = pas + random.choice(list('1234567890')) #Символы, из которых будет составлен пароль
+print('Hello, ', num, 'your password is: ', pas)
 
