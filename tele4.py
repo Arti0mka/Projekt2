4d39c5f62094fd07aacd5e9ab51dfe2835bcb983
Arti0mka <74427717+Arti0mka@users.noreply.github.com>
2021-08-02 19:56:19 +0300

    Update tele4.py

--- tele4.py
+++ tele4.py
@@ -4,8 +4,6 @@ pas = ''
 length = input('длина пароля?'+ "\n")
 length = int(length)
 for x in range(length): #Количество символов (length)
-    pas = pas + random.choice(list('1234567890abcdefghijklmnopqrstuvwxyz')) #Символы, из которых будет составлен пароль
-    pas = input('Введите символы которые хотите использовать?'+ "\n")
-    pas = (pas) 
+    pas = pas + random.choice(list('1234567890abcdefghijklmnopqrstuvwxyz')) #Символы, из которых будет составлен пароль 
 print('Hello, ', num, 'your password is: ', pas) 
 
